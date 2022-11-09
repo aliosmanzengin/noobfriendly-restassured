e2ff9d64bcfc5d6f7c04a1bb646910c1e807f61b
Zengin, Ali Osman <aliosmanzengin0212@gmail.com>
2022-11-09 17:33:23 +0100

    changing header for Accept XML

--- src/test/java/com/herokuapp/restfulbooker/GetBookingTests.java
+++ src/test/java/com/herokuapp/restfulbooker/GetBookingTests.java
@@ -2,6 +2,7 @@ package com.herokuapp.restfulbooker;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.Header;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
@@ -11,7 +12,7 @@ import org.testng.asserts.SoftAssert;
 
 public class GetBookingTests extends BaseTest{
 
-    @Test
+    @Test(enabled = false)
     public void getBookingByIdTest() {
         Response responseCreate = createBooking();
         responseCreate.prettyPrint();
@@ -45,6 +46,45 @@ public class GetBookingTests extends BaseTest{
         softy.assertEquals(actualCheckinDate, "2022-02-02", "checkin date is not matching");
         softy.assertEquals(actualCheckoutDate, "2022-03-03", "checkout date is not matching");
         softy.assertEquals(actualAdditionalNeeds,"W");
+        softy.assertAll();
+    }
+
+    @Test
+    public void getBookingXMLTest() {
+        Response responseCreate = createBooking();
+        responseCreate.prettyPrint();
+
+        //getting booking id to use it
+        // when doing update operation
+        int id = responseCreate.jsonPath().getInt("bookingid");
+
+        //Set path parameter
+        spec.pathParam("bookingId", id);
+        Header xml = new Header("Accept", "application/xml");
+        spec.header(xml);
+
+        Response response = RestAssured.given().spec(spec).get("/booking/{bookingId}");
+        response.prettyPrint();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+
+        String actualLastName = response.xmlPath().getString("booking.lastname");
+        String actualFirstName = response.xmlPath().getString("booking.firstname");
+        int price = response.xmlPath().getInt("booking.totalprice");
+        boolean isDepositPaid = response.xmlPath().getBoolean("booking.depositpaid");
+        String actualCheckinDate = response.xmlPath().getString("booking.bookingdates.checkin");
+        String actualCheckoutDate = response.xmlPath().getString("booking.bookingdates.checkout");
+        String actualAdditionalNeeds = response.xmlPath().getString("booking.additionalneeds");
+
+        //Verify all fields with soft assertion
+        SoftAssert softy = new SoftAssert();
+        softy.assertEquals(actualFirstName, "Ali", "firstname is not matching");
+        softy.assertEquals(actualLastName, "Zengin", "lastname is not matching");
+        softy.assertEquals(price, 199, "price is not correct");
+        softy.assertTrue(isDepositPaid);
+        softy.assertEquals(actualCheckinDate, "2022-02-02", "checkin date is not matching");
+        softy.assertEquals(actualCheckoutDate, "2022-03-03", "checkout date is not matching");
+        softy.assertEquals(actualAdditionalNeeds,"air conditioner");
         softy.assertAll();
     }
 }
